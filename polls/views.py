2a39c120e622d06e44972b72cd140b5c349ea69c
Kim <kimchimik@gmail.com>
2019-01-28 06:32:40 +0000

    fixed imports

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Question
+
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
